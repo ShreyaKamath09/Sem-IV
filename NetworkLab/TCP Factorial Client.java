f2a96415f2b138e40c9142a925210b3ac7e9b6dd
Shreya Kamath <113795442+ShreyaKamath09@users.noreply.github.com>
2023-04-26 18:49:59 +0530

    Create TCP Factorial Client.java

---
+++ NetworkLab/TCP Factorial Client.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+public class Client{
+	public static void main(String[] args)throws IOException{
+		String serverHostname = "localhost";
+		int serverPort = 8080;
+		Socket socket = new Socket(serverHostname, serverPort);
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		
+		String num1;
+		while(true)
+		{
+			System.out.println("Enter a number:");
+			num1 = stdin.readLine();
+			
+			out.println(num1);
+			
+			if (num1.equals("exit")) {
+                break;
+            }
+			
+			String response = in.readLine();
+			System.out.println("Factorial: "+response);
+		}
+	}
+}
