a97dd5a11ce990bf9b6eb20af97b050f9b99e80d
Shreya Kamath <113795442+ShreyaKamath09@users.noreply.github.com>
2023-05-01 12:03:42 +0530

    Create add.asm
    16 Bit BCD Addition

---
+++ MPLab/add.asm
@@ -0,0 +1,44 @@
+.model small
+
+.data
+a dw 3629H
+b dw 4738H
+
+.code
+mov ax, @data
+mov ds, ax
+
+mov ax, a
+mov bx, b
+
+add al, bl
+daa
+mov bl, al
+
+adc ah, bh
+mov al, ah
+daa
+mov bh, al
+
+mov ch, 04h
+mov cl, 04h
+
+l2:
+rol bx, cl
+mov dl, bl
+and dl, 0fH
+cmp dl, 09
+jbe l4
+add dl, 07
+
+l4:
+add dl, 30H
+mov ah, 02
+int 21H
+dec ch
+jnz l2
+
+mov ah, 4ch
+int 21H
+end
+
